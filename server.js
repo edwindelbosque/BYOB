7e272f6021b08523c59e1b957cf8b4fc5a28e767
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-19 16:33:41 -0700

    Add server.js and install packages

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.static('public'));
+
+app.locals.title = 'Film Directors';
+app.locals.films = [
+	{
+		production_year: 2009,
+		id: 4105528,
+		title: 'Planet 51',
+		director: 'Abad, Javier',
+		director_id: 1975,
+		co_directors: 'Abad, Javier|Blanco, Jorge|Martínez, Marcos',
+		co_directors_id: '1516774|1975|233690',
+		genres: 'Adventure|Animation|Comedy|Family|Sci-Fi',
+		cameras: '',
+		negative_format: 'Digital',
+		film_type: 'D'
+	},
+	{
+		production_year: 2006,
+		id: 3973646,
+		title: 'Mission: Impossible III',
+		director: 'Abrams, J.J.',
+		director_id: 7718,
+		co_directors: 'Abrams, J.J.',
+		co_directors_id: 7718,
+		genres: 'Action|Adventure|Thriller',
+		cameras:
+			'Arriflex 235, Panavision C-Series Lenses|Arriflex 435, Panavision Primo, C-Series and Angenieux Optimo Lenses|Panavision Panaflex Millennium XL, Panavision Primo, C-Series and Angenieux Optimo Lenses|Panavision Panaflex Platinum, Panavision Primo, C-Series and Angenieux Optimo Lenses|Sony CineAlta HDC-F950, Zeiss DigiZoom and Fujinon C-Series Lenses',
+		negative_format: '35 mm|Video',
+		film_type: 'D|F'
+	}
+];
+
+app.get('/api/v1/films', (request, response) => {
+	const { films } = app.locals;
+	response.json({ films });
+});
+
+app.listen(app.get('port'), () => {
+	console.log(
+		`${app.locals.title} is running on http://localhost:${app.get('port')}.`
+	);
+});
