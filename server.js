c985864d7c82b7c7acaeb88b63ef7ca22ce776d2
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-11-25 02:22:22 -0700

    Write endpoints for specific sources

--- server.js
+++ server.js
@@ -10,35 +10,6 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.locals.title = 'Film Directors';
-app.locals.films = [
-	{
-		production_year: 2009,
-		id: 4105528,
-		title: 'Planet 51',
-		director: 'Abad, Javier',
-		director_id: 1975,
-		co_directors: 'Abad, Javier|Blanco, Jorge|Martínez, Marcos',
-		co_directors_id: '1516774|1975|233690',
-		genres: 'Adventure|Animation|Comedy|Family|Sci-Fi',
-		cameras: '',
-		negative_format: 'Digital',
-		film_type: 'D'
-	},
-	{
-		production_year: 2006,
-		id: 3973646,
-		title: 'Mission: Impossible III',
-		director: 'Abrams, J.J.',
-		director_id: 7718,
-		co_directors: 'Abrams, J.J.',
-		co_directors_id: 7718,
-		genres: 'Action|Adventure|Thriller',
-		cameras:
-			'Arriflex 235, Panavision C-Series Lenses|Arriflex 435, Panavision Primo, C-Series and Angenieux Optimo Lenses|Panavision Panaflex Millennium XL, Panavision Primo, C-Series and Angenieux Optimo Lenses|Panavision Panaflex Platinum, Panavision Primo, C-Series and Angenieux Optimo Lenses|Sony CineAlta HDC-F950, Zeiss DigiZoom and Fujinon C-Series Lenses',
-		negative_format: '35 mm|Video',
-		film_type: 'D|F'
-	}
-];
 
 app.get('/api/v1/films', (request, response) => {
 	database('films')
@@ -48,6 +19,43 @@ app.get('/api/v1/films', (request, response) => {
 		})
 		.catch(error => {
 			response.status(500).json({ error });
+		});
+});
+
+app.get('/api/v1/directors', (request, response) => {
+	database('directors')
+		.select()
+		.then(directors => {
+			response.status(200).json(directors);
+		})
+		.catch(error => {
+			response.status(500).json({ error });
+		});
+});
+
+app.get('/api/v1/directors/:id', (request, response) => {
+	const { id } = request.params;
+	database('directors')
+		.select()
+		.where({ id: id })
+		.then(director => {
+			if (!director.length) {
+				response.status(404).json(`There is no director with id ${id}`);
+			}
+			response.status(200).json(director);
+		});
+});
+
+app.get('/api/v1/films/:id', (request, response) => {
+	const { id } = request.params;
+	database('films')
+		.select()
+		.where({ id: id })
+		.then(film => {
+			if (!film.length) {
+				response.status(404).json(`There is no film with id ${id}`);
+			}
+			response.status(200).json(film);
 		});
 });
 
